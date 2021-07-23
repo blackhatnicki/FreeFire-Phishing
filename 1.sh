e6546a05840e38df6dc451835f0a92e375f481be
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2021-07-23 07:21:58 +0530

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo ""
+clear
+eval(base64_decode('ZWNobyAtZSAiIFxlWzE7MzNtW1xlWzBtXGVbMTs3N21+XGVbMG1cZVsxOzMzbV1cZVswbVxlWzE7MzJtIEluc3RhbGxpbmcgVGhlIEZyZWUgRmlyZSBQaGlzaGluZyBUb29sIFBsZWFzZSBXYWl0Li4uLiAgXGVbMG0i'));
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!             KEY REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e "\e[33m
+         
+         ██╗  ██╗███████╗██╗   ██╗
+         ██║ ██╔╝██╔════╝╚██╗ ██╔╝
+         █████╔╝ █████╗   ╚████╔╝ 
+         ██╔═██╗ ██╔══╝    ╚██╔╝  
+         ██║  ██╗███████╗   ██║   
+         ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/OHSMFF  "
+echo ""
+echo ""
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Admin Activation Key : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'9DY7VHT1JKVBMYJC'* ]]; then
+clear
+echo ""
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Free Fire Phishing Tool Please Wait....  \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
+echo ""
+exit
